17d80d2f59c9ed43d4fa1a1d7011bbcd84784738
htack210 <htack210@live.com>
2024-06-12 19:28:03 -0400

    enhancing list and profile

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -5,9 +5,17 @@ import { useGlobalState } from "@/app/context/globalProvider";
 import Image from "next/image";
 import menu from "@/app/utils/menu";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 function Sidebar() {
   const { theme } = useGlobalState();
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleClick = (link: string) => {
+    router.push(link);
+  };
 
   return (
     <SidebarStyled theme={theme}>
@@ -24,14 +32,23 @@ function Sidebar() {
 
       <ul className="list-none">
         {menu.map((item) => {
+          const link = item.link;
+          const title = item.title;
+          const id = item.id;
+
           return (
-            <li className="nav-item">
+            <li
+              className={`nav-item ${pathname === link ? "active" : ""}`}
+              key={id}
+              onClick={() => handleClick(link)}
+            >
               {item.icon}
-              <Link href={item.link}>{item.title}</Link>
+              <Link href={link}>{title}</Link>
             </li>
           );
         })}
       </ul>
+      <button>I'm not ready yet!</button>
     </SidebarStyled>
   );
 }
@@ -42,6 +59,42 @@ const SidebarStyled = styled.nav`
   background-color: ${(props) => props.theme.colorBg2};
   border: 2px solid ${(props) => props.theme.borderColor2};
   border-radius: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  color: ${(props) => props.theme.colorGrey3};
+
+  .profile {
+    margin: 1.5rem;
+    padding: 1rem 0.8rem;
+    position: relative;
+    border-radius: 1rem;
+    cursor: pointer;
+
+    font-weight: 500;
+    color: ${(props) => props.theme.colorGrey0};
+
+    display: flex;
+    align-items: center;
+  }
+
+  .profile-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    backdrop-filter: blur(10px);
+    z-index: 0;
+    background: ${(props) => props.theme.colorBg3};
+    transition: all 0.55s linear;
+    border-radius: 1rem;
+    border: 2px solid ${(props) => props.theme.borderColor2};
+
+    opacity: 0.2;
+  }
 `;
 
 export default Sidebar;
