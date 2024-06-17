add4324afb8d637b06b5956f956257e8eb0352ad
htack210 <htack210@live.com>
2024-06-17 19:26:06 -0400

    Adding dynamic user info.

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -3,15 +3,24 @@ import React from "react";
 import styled from "styled-components";
 import { useGlobalState } from "@/app/context/globalProvider";
 import Image from "next/image";
+
 import menu from "@/app/utils/menu";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
-import { useClerk } from "@clerk/nextjs";
-import { logout } from "@/app/utils/Icons";
-import Button from "../Button/Button";
+import { arrowLeft, bars, logout } from "@/app/utils/Icons";
+import { useClerk, UserButton, useUser } from "@clerk/nextjs";
 
 function Sidebar() {
   const { theme } = useGlobalState();
+  const { signOut } = useClerk();
+
+  const { user } = useUser();
+
+  const { firstName, lastName, imageUrl } = user || {
+    firstName: "",
+    lastName: "",
+    imageUrl: "",
+  };
 
   const router = useRouter();
   const pathname = usePathname();
@@ -19,18 +28,21 @@ function Sidebar() {
   const handleClick = (link: string) => {
     router.push(link);
   };
-  const { signOut } = useClerk();
 
   return (
     <SidebarStyled theme={theme}>
       <div className="profile">
         <div className="profile-overlay"></div>
-        <div>
-          <Image src="/me.png" width={70} height={70} alt="profile pic" />
+        <div className="image">
+          <Image width={70} height={70} src={imageUrl} alt="profile pic" />
         </div>
-        <h1>
-          <span>Me </span>
-          <span>Myself</span>
+        <div className="user-btn absolute z-20 top-0 w-full h-full">
+          <UserButton />
+        </div>
+        <h1 className="capitalize ml-2">
+          {firstName}
+          <br />
+          {lastName}
         </h1>
       </div>
 
@@ -91,6 +103,23 @@ const SidebarStyled = styled.nav`
   justify-content: space-between;
 
   color: ${(props) => props.theme.colorGrey3};
+
+  .user-btn {
+    .cl-userButtonBox {
+      width: 100%;
+      height: 100%;
+    }
+      .cl-rootBox {
+        width: 100%;
+        height: 100%;
+        .cl-userButtonTrigger {
+          width: 100%;
+          height: 100%;
+          opacity: 0;
+        }
+      }
+    }
+  }
 
   .profile {
     margin: 1.5rem;
@@ -154,7 +183,7 @@ const SidebarStyled = styled.nav`
 
     > h1 {
       margin-left: 0.8rem;
-      font-size: clamp(1.2rem, 4vw, 1.4rem);
+      font-size: clamp(1.2rem, 4vw, 1.2rem);
       line-height: 100%;
     }
 
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    domains: [
+      "localhost",
+      "https://img.clerk.com",
+      "img.clerk.com",
+      "https://unsplash.com/",
+    ],
+  },
 };
 
 export default nextConfig;
